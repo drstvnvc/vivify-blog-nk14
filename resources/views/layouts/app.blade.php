868cb51f0d27aad71cf9f7cdc2cba8ec78409319
Drago Stevanovic <drago@sounder.fm>
2021-07-22 18:23:08 +0200

    Display error messages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,8 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
     <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script> -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
     <!-- Styles
     <style>
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,15 +3,29 @@
 @section('title', 'Create a post')
 
 @section('content')
+
+@if ($errors->any())
+<div class="alert alert-danger">
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
+
 <form method="POST" action="/posts">
     @csrf
-    <div class="form-group">
+    <div class="form-group has-validation">
         <label for="title">Title</label>
         <input type="text" name="title" class="form-control" id="title" placeholder="Title">
+        @include('partials.error-message', ['field' => 'title'])
     </div>
-    <div class="form-group">
+    <div class="form-group has-validation">
         <label for="body">Body</label>
         <textarea name="body" class="form-control" id="body" rows="5" placeholder="Write your post here..."></textarea>
+        @include('partials.error-message', ['field' => 'body'])
+
     </div>
     <div class="form-check">
         <input type="checkbox" name="is_published" class="form-check-input" id="is_published" value="1">
